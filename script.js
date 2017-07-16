a4408ea605b4795e85664bb868456cbe92beceaf
berkly <daddyrusher@gmail.com>
2017-07-16 10:56:08 +0500

    Calc 1.0

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    var calc = $('.calculator');
+    var calcDisplay = calc.find('.calculator__display');
+    var calcKeys = calc.find('.calculator__key');
+    var calcButton = calc.find('.calculator__button');
+    var calcClear = calc.find('.calculator__clear');
+    var calcEqual = calc.find('.calculator__key--equal');
+    var calcPower = calc.find('.calculator__power');
+    var calcSpace = calc.find('.calculator__backspace');
+
+    calcKeys.each(function () {
+        var current = $(this).attr('value');
+        $(this).text(current);
+    });
+
+    calcButton.on('click', function () {
+        calcDisplay.val( calcDisplay.val() + $(this).attr('value') );
+    });
+
+    calcClear.on('click', function () {
+        calcDisplay.val('');
+    });
+
+    calcEqual.on('click', function () {
+        calcDisplay.val( eval( calcDisplay.val() ) );
+    });
+
+    calcSpace.on('click', function () { 
+        calcDisplay.val( calcDisplay.val().substring(0, calcDisplay.val().length-1) );
+    });
+
+});
