9ea97571fe9e37fc8224c0ff98196ada3dcf95fb
berkly <daddyrusher@gmail.com>
2017-07-17 01:43:24 +0500

    Final version

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
     var calcDisplay = calc.find('.calculator__display');
     var calcKeys = calc.find('.calculator__key');
     var calcButton = calc.find('.calculator__button');
-    var calcClear = calc.find('.calculator__clear');
+    var calcClear = calc.find('.calculator__c');
     var calcEqual = calc.find('.calculator__key--equal');
-    var calcPower = calc.find('.calculator__power');
-    var calcSpace = calc.find('.calculator__backspace');
+    var calcPersent = calc.find('.calculator__persent');
+    var calcSpace = calc.find('.calculator__ce');
 
     calcKeys.each(function () {
         var current = $(this).attr('value');
@@ -28,6 +28,10 @@ $(document).ready(function () {
 
     calcSpace.on('click', function () { 
         calcDisplay.val( calcDisplay.val().substring(0, calcDisplay.val().length-1) );
+    });
+
+    calcPersent.on('click', function() {
+        calcDisplay.val(calcDisplay.val() / 100);
     });
 
 });
